ca06c2d4db051db3cfe232901d38a2e2285aa06a
mightykiddo <93716044+mightykiddo@users.noreply.github.com>
2024-03-15 17:58:10 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,11 @@ const router = express.Router();
 
 const path = require('path');
 
+// get landing page
+router.get("/", function(req,res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
 // get main page
 router.get("/mainpage", function(req,res) {
   res.sendFile(path.join(__dirname, '../index.html'));
